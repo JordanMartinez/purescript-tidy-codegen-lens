239b983bee26f01725087d2cb3a651197c18c4f9
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-01-03 11:58:48 -0800

    Drop 'ReimportWithHiding'
    This file does not compile. The compiler thinks the
    OpenImportNameClashes 'Ordering' type still clashes
    with Prelude's Ordering type.

--- snapshots/CheckOpenImports/ReimportWithHiding.purs
+++ snapshots/CheckOpenImports/ReimportWithHiding.purs
@@ -3,11 +3,11 @@ module Snapshots.CheckOpenImports.ReimportWitHiding where
 -- This open import should be re-imported
 -- since the 'OpenImportNameClashes.Ordering'
 -- type's name clashes with the `Prelude.Ordering` type's name
-import Snapshots.Imports.OpenImportNameClashes hiding (Ordering(..))
+-- import Snapshots.Imports.OpenImportNameClashes hiding (Ordering)
 
-import Prelude
+-- import Prelude
 
-data One = One
-data Two = Two
+-- data One = One
+-- data Two = Two
 
-data Foo = Foo One Two Ordering
+-- data Foo = Foo One Two Ordering
--- snapshots/CheckOpenImports/ReimportWithHiding/Lens.purs
+++ snapshots/CheckOpenImports/ReimportWithHiding/Lens.purs
@@ -1,31 +1,31 @@
 module Snapshots.CheckOpenImports.ReimportWitHiding.Lens where
 
-import Prelude
+-- import Prelude
 
-import Snapshots.Imports.OpenImportNameClashes hiding (Ordering)
+-- import Snapshots.Imports.OpenImportNameClashes hiding (Ordering)
 
-import Data.Lens (Lens')
-import Data.Lens.Iso (iso)
-import Data.Lens.Record (prop)
-import Prelude (Unit, const, unit)
-import Snapshots.CheckOpenImports.ReimportWitHiding (Foo(..), One(..), Two(..))
-import Type.Proxy (Proxy(..))
+-- import Data.Lens (Lens')
+-- import Data.Lens.Iso (iso)
+-- import Data.Lens.Record (prop)
+-- import Prelude (Unit, const, unit)
+-- import Snapshots.CheckOpenImports.ReimportWitHiding (Foo(..), One(..), Two(..))
+-- import Type.Proxy (Proxy(..))
 
-_One :: Lens' One Unit
-_One = iso (const unit) (const One)
+-- _One :: Lens' One Unit
+-- _One = iso (const unit) (const One)
 
-_Two :: Lens' Two Unit
-_Two = iso (const unit) (const Two)
+-- _Two :: Lens' Two Unit
+-- _Two = iso (const unit) (const Two)
 
-_Foo :: Lens' Foo { arg1 :: One, arg2 :: Two, arg3 :: Ordering }
-_Foo = iso (\(Foo arg1 arg2 arg3) -> { arg1: arg1, arg2: arg2, arg3: arg3 })
-  \{ arg1, arg2, arg3 } -> Foo arg1 arg2 arg3
+-- _Foo :: Lens' Foo { arg1 :: One, arg2 :: Two, arg3 :: Ordering }
+-- _Foo = iso (\(Foo arg1 arg2 arg3) -> { arg1: arg1, arg2: arg2, arg3: arg3 })
+--   \{ arg1, arg2, arg3 } -> Foo arg1 arg2 arg3
 
-_propArg1 :: forall r a. Lens' { arg1 :: a | r } a
-_propArg1 = prop (Proxy :: Proxy "arg1")
+-- _propArg1 :: forall r a. Lens' { arg1 :: a | r } a
+-- _propArg1 = prop (Proxy :: Proxy "arg1")
 
-_propArg2 :: forall r a. Lens' { arg2 :: a | r } a
-_propArg2 = prop (Proxy :: Proxy "arg2")
+-- _propArg2 :: forall r a. Lens' { arg2 :: a | r } a
+-- _propArg2 = prop (Proxy :: Proxy "arg2")
 
-_propArg3 :: forall r a. Lens' { arg3 :: a | r } a
-_propArg3 = prop (Proxy :: Proxy "arg3")
+-- _propArg3 :: forall r a. Lens' { arg3 :: a | r } a
+-- _propArg3 = prop (Proxy :: Proxy "arg3")
