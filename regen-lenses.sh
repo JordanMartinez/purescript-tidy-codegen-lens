18e8d593df2f0825737aa4003ad9680b5b328351
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-01-07 16:41:07 -0800

    Update regen-lenses args to drop non-module args

--- regen-lenses.sh
+++ regen-lenses.sh
@@ -6,7 +6,5 @@ rm -rf lenses
   --output-dir lenses \
   --gen-type-alias-isos \
   --label-prefix-none \
-  --global-record-lens-overwrite-file \
-  --global-record-lens-file lenses/RecordLens.purs \
   --global-record-lens-module Dependencies.RecordLens \
   .spago/*/*/src/**/*.purs:4
